066c4cd7bfd0c28d63065619da6951d4d188017b
Yoon Seokjin <craftyworks@gmail.com>
2018-06-06 16:04:14 +0900

    edit

--- src/main/java/com/honsul/inthewood/bot/kakao/model/Keyboard.java
+++ src/main/java/com/honsul/inthewood/bot/kakao/model/Keyboard.java
@@ -1,8 +1,12 @@
 package com.honsul.inthewood.bot.kakao.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 import lombok.Data;
 
 @Data
+@JsonInclude(Include.NON_NULL)
 public class Keyboard {
   private String type;
   private String[] buttons;
