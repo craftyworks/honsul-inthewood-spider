2064961dbec3864c20aba240c620535a1f277d03
Yoon Seokjin <craftyworks@gmail.com>
2018-06-13 19:54:57 +0900

    edit

--- src/main/java/com/honsul/inthewood/bot/slack/web/HugoSlackBotController.java
+++ src/main/java/com/honsul/inthewood/bot/slack/web/HugoSlackBotController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,19 +16,19 @@ public class HugoSlackBotController {
   
   private final Logger logger = LoggerFactory.getLogger(getClass());
   
-  @PostMapping("action")
+  @PostMapping(value = "action", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
   public Map actionCommand(@RequestBody Map request) {
     logger.info("received incoming message : {}", request);
     return request;
   }
   
-  @PostMapping("actionMenu")
+  @PostMapping(value = "actionMenu", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
   public Map menuAction(@RequestBody Map request) {
     logger.info("received incoming menu : {}", request);
     return request;
   }  
   
-  @PostMapping("slash")
+  @PostMapping(value = "slash", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
   public Map slashCommand(@RequestBody Map request) {
     logger.info("received slash command : {}", request);
     return request;
