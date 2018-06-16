72e8693250d9d7a2d387ef629320211e90d66ae5
Yoon Seokjin <craftyworks@gmail.com>
2018-06-16 17:59:57 +0900

    로그포맷 수정. Slack Bot Controller 작업중

---
+++ src/main/java/com/honsul/inthewood/bot/slack/model/SlackSlashCommand.java
@@ -0,0 +1,33 @@
+package com.honsul.inthewood.bot.slack.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+
+@Data
+public class SlackSlashCommand {
+  private String token;
+  private String command;
+  private String text;
+
+  @JsonProperty("team_id")
+  private String teamId;
+
+  @JsonProperty("team_domain")
+  private String teamDomain;
+
+  @JsonProperty("channel_id")
+  private String channelId;
+
+  @JsonProperty("channel_name")
+  private String channelName;
+
+  @JsonProperty("user_id")
+  private String userId;
+
+  @JsonProperty("user_name")
+  private String userName;
+
+  @JsonProperty("response_url")
+  private String responseUrl;
+}
--- src/main/java/com/honsul/inthewood/bot/slack/web/HugoSlackBotController.java
+++ src/main/java/com/honsul/inthewood/bot/slack/web/HugoSlackBotController.java
@@ -1,7 +1,6 @@
 package com.honsul.inthewood.bot.slack.web;
 
 import java.util.Enumeration;
-import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -9,11 +8,13 @@ import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.honsul.inthewood.bot.slack.model.SlackSlashCommand;
 
 @RestController
 @RequestMapping("/bot/slack/hugo")
@@ -38,36 +39,15 @@ public class HugoSlackBotController {
   }  
   
   @PostMapping(value = "slash", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-  public Map onReceiveSlashCommand(@RequestParam("token") String token,
-                                       @RequestParam("team_id") String teamId,
-                                       @RequestParam("team_domain") String teamDomain,
-                                       @RequestParam("channel_id") String channelId,
-                                       @RequestParam("channel_name") String channelName,
-                                       @RequestParam("user_id") String userId,
-                                       @RequestParam("user_name") String userName,
-                                       @RequestParam("command") String command,
-                                       @RequestParam("text") String text,
-                                       @RequestParam("response_url") String responseUrl, HttpServletRequest request) {
-      
-    logger.info(token+","+teamId +"," + teamDomain + ", " + channelId + ", " + userId + ", " + userName + ", " + command + ", " + text + ", " + responseUrl);
-    Enumeration<String> enums = request.getParameterNames();
-    while(enums.hasMoreElements()) {
-      String key = enums.nextElement();
-      logger.info("received params : {} : {}", key, request.getParameter(key));
-    }
+  public SlackSlashCommand onReceiveSlashCommand(@ModelAttribute SlackSlashCommand slashCommand) {
+    logger.info("received slash command : {}", slashCommand);
     
-    return new HashMap<>();
+    return slashCommand;
   }
 
-  @PostMapping(value = "slash2", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-  public Map slashCommand(Map payload, HttpServletRequest request) {
-    Enumeration<String> enums = request.getParameterNames();
-    while(enums.hasMoreElements()) {
-      String key = enums.nextElement();
-      logger.info("received params : {} : {}", key, request.getParameter(key));
-    }
-    logger.info("received incoming message : {}", payload);
-    logger.info("received slash command : {}", request);
-    return payload;
+  @PostMapping(value = "event")
+  public Map<String, Object> onEvent(@RequestBody Map<String, Object> event) {
+    logger.info("received event : {}", event);
+    return event;
   }    
 }
