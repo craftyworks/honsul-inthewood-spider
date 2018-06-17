d5750d9d9be3f73bfafed7e09fd86dcdf4381d08
Yoon Seokjin <craftyworks@gmail.com>
2018-06-17 22:31:45 +0900

    알림 중지 확인 메시지

--- src/main/java/com/honsul/inthewood/bot/slack/action/StopNotificationActionCommandHandler.java
+++ src/main/java/com/honsul/inthewood/bot/slack/action/StopNotificationActionCommandHandler.java
@@ -1,9 +1,12 @@
 package com.honsul.inthewood.bot.slack.action;
 
+import java.util.Arrays;
+
 import org.springframework.stereotype.Component;
 
 import com.honsul.inthewood.bot.slack.SlackBotAPI;
 import com.honsul.inthewood.bot.slack.model.SlackActionCommand;
+import com.honsul.inthewood.bot.slack.model.SlackAttachment;
 import com.honsul.inthewood.bot.slack.model.SlackMessage;
 
 @Component
@@ -15,7 +18,17 @@ public class StopNotificationActionCommandHandler extends ActionCommandHandler {
 
   @Override
   public void execute(SlackBotAPI api, SlackActionCommand command) {
-    api.sendMessage(command.getResponseUrl(), SlackMessage.builder().text("처리 완료!").build());
+    SlackMessage message = command.getOriginalMessage();
+    message.setAttachments(new SlackAttachment[] {
+        SlackAttachment.builder()
+        .text("휴양림 예약현황 알림 서비스가 중지되었습니다.\n중지된 예약현황 알림 서비스를 재시작하려면 */start* 명령을 사용하세요.")
+        .markdownIn(Arrays.asList(new String[] {"text"}))
+        .color("#3AA3E3")
+        .attachmentType("default")
+        .build()
+    });
+
+    api.sendMessage(command.getResponseUrl(), message);
   }
 
 }
