88ba2c33d1866d92dd7164fe5037789387a02d6f
Yoon Seokjin <craftyworks@gmail.com>
2018-06-23 20:53:55 +0900

    edit

--- src/main/java/com/honsul/inthewood/bot/slack/SlackBotController.java
+++ src/main/java/com/honsul/inthewood/bot/slack/SlackBotController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
 import com.honsul.inthewood.bot.slack.action.ActionCommandHandler;
@@ -78,6 +79,7 @@ public class SlackBotController {
   /** 
    * Slack Slash Command Handler 
    */
+  @ResponseBody
   @PostMapping(value = "slash", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
   public SlackMessage onReceiveSlashCommand(@RequestBody SlackSlashCommand slashCommand) {
     logger.info("received slash command : {}", slashCommand);
@@ -100,6 +102,7 @@ public class SlackBotController {
    * Slack Event Handler
    */
   @PostMapping(value = "event")
+  @ResponseBody
   public SlackEventMessage onEvent(@RequestBody SlackEventMessage eventMessage) {
     logger.info("received event : {}", eventMessage);
     return eventMessage;
