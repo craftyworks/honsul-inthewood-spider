123d307a79ee78f6031afafb5c6333e2fc7b3de3
HAN <HAN@HAN-PC>
2018-06-29 00:55:03 +0900

    push r026 to server

--- src/main/java/com/honsul/inthewood/spider/collector/r026/R026BookingParser.java
+++ src/main/java/com/honsul/inthewood/spider/collector/r026/R026BookingParser.java
@@ -5,10 +5,14 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.DataNode;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import com.honsul.inthewood.core.SpiderContext;
 import com.honsul.inthewood.core.annotation.BookingParser;
@@ -27,21 +31,30 @@ public class R026BookingParser extends JsoupBookingParser {
   @Override
   protected List<Document> documents() throws IOException {
     List<Document> documentList = new ArrayList<>();    
-    
+        
     Document dummy = Jsoup.connect(MAIN_URL).get();
     String contentUid = dummy.select("#gnb_menu > li.gm03 > div > ul > li:nth-child(2) > a[href]").attr("href").split("=")[1];
+	
     dummy = Jsoup.connect(MAIN_URL).data("contentUid", contentUid).get();
-    
     String year = String.valueOf(LocalDate.now().getYear());
-    String month = String.valueOf(LocalDate.now().getMonthValue());
+    String month = String.format("%02d", LocalDate.now().getMonthValue());
     String nextYear = String.valueOf(LocalDate.now().plusMonths(1).getYear());
-    String nextMonth = String.valueOf(LocalDate.now().plusMonths(1).getMonthValue());
+    String nextMonth = String.format("%02d", LocalDate.now().plusMonths(1).getMonthValue());
+    
+	Elements scripts = dummy.getElementsByTag("script");
+	for (Element element : scripts) {
+		for (DataNode node : element.dataNodes()) {
+			String content = node.getWholeData();
+			if (content.contains("contentUid")) {
+				contentUid = content.split("contentUid=")[1].split("&year")[0];
+				break;
+			}
+        }
+	}
 	
     String key = dummy.select("#reserve > ul > li:nth-child(1) > a:nth-child(5)").attr("onclick").split("\\'\\)")[0];
     String categoryUid1 = key.split("','")[1];
     String categoryUid1s = key.split("','")[2];		
-    contentUid = dummy.select("#leftArea > div > ul > li.on > a").attr("href").split("=")[1];
-    
     documentList.add(Jsoup.connect(CONNECT_URL).data("contentUid", contentUid).data("year", year).data("month", month).data("categoryUid1", categoryUid1).data("categoryUid1s", categoryUid1s).get());
     documentList.add(Jsoup.connect(CONNECT_URL).data("contentUid", contentUid).data("year", nextYear).data("month", nextMonth).data("categoryUid1", categoryUid1).data("categoryUid1s", categoryUid1s).get());
     return documentList;
@@ -51,21 +64,31 @@ public class R026BookingParser extends JsoupBookingParser {
   public List<Booking> extract(Document doc) {
     List<Booking> bookingList = new ArrayList<>(); 
 
-    String yearMonth = doc.select("#contents > div.reservation > h3").text();
-    yearMonth = yearMonth.replace("년", "-").replaceAll("월|\\s", "");    
+    Elements scripts = doc.getElementsByTag("script");
     
-    for(Element td : doc.select("#contents > div.reservation > table > tbody > tr > td")) {
-    	if(td.text().length() == 0 || td.text().contains("예약종료")) {
-	          continue;
-	    }
-    	for(Element form : td.select("form")) {
-        	if(td.text().length() == 0 || td.text().contains("예약종료")) {
-	          continue;
-	        }
-        	String[] attr = form.select("input[name='rsv_info']").attr("value").split("#@");
-            String bookingDt = attr[2];
-            String roomNm = form.text().replace("*",""); // remove '*'
-        	
+    for(Element tr : doc.select("#content > table > tbody > tr")) {
+    	if(tr.select("td").size() == 0) {continue;}
+    	
+    	String roomNm = tr.select("th").text().split("\\(")[0].replaceAll("\\s|\\t", "");
+    	
+    	for(Element td : tr.select("td")) {   
+    		Boolean findFlag = false;
+    		outerloop:
+    		for (Element element : scripts) {
+    			for (DataNode node : element.dataNodes()) {
+    				String content = node.getWholeData();    				
+    				String pattern = "\\b"+td.attr("id")+"\\b";
+    			    Pattern p = Pattern.compile(pattern);
+    			    Matcher m = p.matcher(content);
+    			    if (m.find()) {
+    					findFlag = true;
+    					break outerloop;
+    				}
+    	        }
+    		}
+    		if(td.select("a[href]").size() == 0 || findFlag == true) {continue;}
+    		    		
+        	String bookingDt = td.select("a").attr("href").split("Day=")[1];            
         	Booking booking = new Booking();
 	        booking.setResortId(SpiderContext.getResortId());
 	        booking.setBookingDt(LocalDate.parse(bookingDt, DateTimeFormatter.ofPattern("yyyy-MM-dd")));
--- src/main/java/com/honsul/inthewood/spider/collector/r026/R026RoomParser.java
+++ src/main/java/com/honsul/inthewood/spider/collector/r026/R026RoomParser.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.jsoup.Connection.Response;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -21,46 +22,79 @@ import com.honsul.inthewood.core.util.TextUtils;
 @RoomParser(resortId="R026")
 public class R026RoomParser extends JsoupRoomParser {
   
+	private static final String BASE_URL = "http://gahak.haenam.go.kr";
 	private static final String HUT_URL = "http://gahak.haenam.go.kr/index.9is";
 	  
 	@Override
 	protected List<Document> documents() throws IOException {
 		List<Document> docs = new ArrayList<>();
-		docs.add(Jsoup.connect(HUT_URL).get());    
+		
+		Document dummy = Jsoup.connect(HUT_URL).get();
+		String contentUid = dummy.select("#gnb_menu > li.gm02 > a[href]").attr("href").split("=")[1];
+		Response response = Jsoup.connect(HUT_URL).data("contentUid", contentUid).followRedirects(false).execute();
+		response = Jsoup.connect(response.header("Location")).followRedirects(false).execute();
+		Document doc = Jsoup.connect(response.header("Location")).get();
+		
+		for(Element roomType : doc.select("#leftArea > div > ul > li.on > ul > li")) {
+			contentUid = roomType.select("a").attr("href").split("=")[1];
+			response = Jsoup.connect(HUT_URL).data("contentUid", contentUid).followRedirects(false).execute();
+			doc = Jsoup.connect(response.header("Location")).get();
+			
+			for(Element roomList : doc.select("#content > ul > li")) {
+				String param = roomList.select("a").attr("href");
+				docs.add(Jsoup.connect(BASE_URL+param).get());				
+			}
+		}
+		contentUid = doc.select("#leftArea > div > ul > li:nth-child(2) > a").attr("href").split("=")[1];
+		response = Jsoup.connect(HUT_URL).data("contentUid", contentUid).followRedirects(false).execute();
+		doc = Jsoup.connect(response.header("Location")).get();
+		
+		for(Element roomList : doc.select("#content > ul > li")) {
+			String param = roomList.select("a").attr("href");
+			docs.add(Jsoup.connect(BASE_URL+param).get());				
+		}
+		//System.out.println("docs: " + docs.size());
 		return docs;
 	}
 
 	@Override
 	public List<Room> extract(Document doc) {
-		List<Room> roomList = new ArrayList<>();
+		List<Room> roomList = new ArrayList<>();		
+				
+		String roomNm = doc.select("#content > div.room_title > h4").text().replaceAll("\\s+", "");
+		String space = doc.select("#content > table > tbody > tr > td:nth-child(1)").text();
+		long peakPrice = TextUtils.findMoneyLong(doc.select("#content > table > tbody > tr > td:nth-child(2)").text());
+		long price = TextUtils.findMoneyLong(doc.select("#content > table > tbody > tr > td:nth-child(3)").text());
+		String people;
 		
-		for(Element tr : doc.select("#contents > div.facilities > table:nth-child(4) > tbody > tr")) {
-			String[] roomTitle = tr.select("td:nth-child(1)").text().split(" ");
-			String space = roomTitle[0].contains("물소리") ? "82㎡" : "29.95㎡"; 
-			String people = tr.select("td:nth-child(2)").text().replace("명", "");
-			long price = TextUtils.findMoneyLong(tr.select("td:nth-child(5)").text());
-			long peakPrice = TextUtils.findMoneyLong(tr.select("td:nth-child(3)").text());
-			
-			for (int i=1; i <= Integer.valueOf(roomTitle[1].substring(roomTitle[1].length() - 1)); i++) {
-				String roomNm = roomTitle[0] + String.valueOf(i);
+	    switch(space){
+	        case "22평": 
+	        	people = "12";break;
+	        case "18평":
+	        	people = "10";break;
+	        case "12평":
+	        	people = "6";break;
+	        case "7평" :
+	        	people = "4";break;
+	        default :
+	        	people = "4";
+	    }
+		
+		Room room = new Room();
+	    room.setResortId(SpiderContext.getResortId());
+	    room.setRoomNm(roomNm.trim());
+	    room.setRoomType(getRoomType(roomNm));
+	    room.setSpace(space);
+	    room.setNumberOfPeople(people);
+	    room.setPeakPrice(peakPrice);
+	    room.setPrice(price);
+	    roomList.add(room); 
 
-				Room room = new Room();
-	    	    room.setResortId(SpiderContext.getResortId());
-	    	    room.setRoomNm(roomNm.trim());
-	    	    room.setRoomType(RoomType.HUT);
-	    	    room.setSpace(space);
-	    	    room.setNumberOfPeople(people);
-	    	    room.setPeakPrice(peakPrice);
-	    	    room.setPrice(price);
-	    	    roomList.add(room); 
-    	    }
-		}
-		//System.out.println("roomList: " + roomList);
 		return roomList;
 	}
 	
 	@Override
 	protected RoomType getRoomType(String roomNm) {
-		return RoomType.getRoomType(roomNm);
+		return TextUtils.contains(roomNm, "가리재", "깃대봉", "두억봉", "바람재") ? RoomType.CONDO : RoomType.HUT ;
   }
 }
