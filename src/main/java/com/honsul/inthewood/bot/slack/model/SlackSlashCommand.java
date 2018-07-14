fbe2d8f80dd6f033c1ccddb74dff0097438354fd
Yoon Seokjin <craftyworks@gmail.com>
2018-07-14 15:14:26 +0900

    lombok @Builder 추가

--- src/main/java/com/honsul/inthewood/bot/slack/model/SlackSlashCommand.java
+++ src/main/java/com/honsul/inthewood/bot/slack/model/SlackSlashCommand.java
@@ -3,9 +3,15 @@ package com.honsul.inthewood.bot.slack.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class SlackSlashCommand implements TokenBarer {
   private String token;
