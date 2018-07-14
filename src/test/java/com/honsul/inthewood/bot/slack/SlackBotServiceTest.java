aebabdb47a8657f7050638998d9049980d8ed863
Yoon Seokjin <craftyworks@gmail.com>
2018-07-14 15:16:32 +0900

    selectSlackSubscription 테스트 추가

--- src/test/java/com/honsul/inthewood/bot/slack/SlackBotServiceTest.java
+++ src/test/java/com/honsul/inthewood/bot/slack/SlackBotServiceTest.java
@@ -1,7 +1,7 @@
 package com.honsul.inthewood.bot.slack;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.honsul.inthewood.HonsulInTheWoodApplication;
+import com.honsul.inthewood.bot.slack.model.SlackSlashCommand;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes= {HonsulInTheWoodApplication.class})
@@ -21,12 +22,23 @@ public class SlackBotServiceTest {
   @Test
   public void testGetSlackBotAccessToken() throws Exception {
     String token = service.getSlackBotAccessToken("U0502LPJC");
-    assertThat(token.contains("xoxb"), is(true));
+    assertThat(token).contains("xoxb");
   }
 
   @Test
   public void testGetSlackUserAccessToken() throws Exception {
     String token = service.getSlackUserAccessToken("U0502LPJC");
-    assertThat(token.contains("xoxp"), is(true));
+    assertThat(token).contains("xoxp");
   }
+
+  @Test
+  public void testSelectSlackSubscription() throws Exception {
+    SlackSlashCommand slashCommand = mock(SlackSlashCommand.class);
+    when(slashCommand.getUserId()).thenReturn("U0502LPJC");
+    when(slashCommand.getChannelId()).thenReturn("GB1NY12UX");
+    
+    assertThat(service.selectSlackSubscription(slashCommand)).isNotEmpty();
+  }
+  
+  
 }
